decef910b1ce8d0a12be45b1f2d8a2d12b3a59f2
Owner <ssscappat@gmail.com>
2021-03-15 23:48:11 -0600

    adding coingecko logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="coingecko-logo"
-      src="~assets/coingecko.png"
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
     >
     <zap-bond-widget :address=coinGeckoOracle endpoint="CoinCap.io Datafeed"></zap-bond-widget>
   </q-page>
